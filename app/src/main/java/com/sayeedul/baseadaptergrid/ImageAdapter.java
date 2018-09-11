361b3204737dd9eb62bb236a725085a6fd4a2b74
Sayeedul rahman <sayeedul@users.noreply.github.com>
2018-09-11 20:27:01 +0530

    Initial commit

---
+++ app/src/main/java/com/sayeedul/baseadaptergrid/ImageAdapter.java
@@ -0,0 +1,52 @@
+package com.sayeedul.baseadaptergrid;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    // Keep all Images in array
+    public Integer[] mThumbIds = {
+            R.drawable.alrm, R.drawable.beer,
+            R.drawable.calc, R.drawable.calender,
+            R.drawable.cheese, R.drawable.dollar,
+            R.drawable.eat, R.drawable.gift,
+            R.drawable.insta, R.drawable.iphone,
+            R.drawable.lock, R.drawable.orange,
+    };
+
+    // Constructor
+    public ImageAdapter(Context c){
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mThumbIds[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setImageResource(mThumbIds[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setLayoutParams(new GridView.LayoutParams(100, 100));
+        return imageView;
+    }
+
+}
---
+++ app/src/main/java/com/sayeedul/baseadaptergrid/MainActivity.java
@@ -0,0 +1,42 @@
+package com.sayeedul.baseadaptergrid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.grid_view);
+
+        // Instance of ImageAdapter Class
+        gridView.setAdapter(new ImageAdapter(this));
+
+        /**
+         * On Click event for Single Gridview Item
+         * */
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                // Sending image id to FullScreenActivity
+                Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
+                // passing array index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+
+    }
+
+
+
+}
